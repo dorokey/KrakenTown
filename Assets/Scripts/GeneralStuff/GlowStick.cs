ee555f89d0296fd375c9423cfd08a7591a191a30
dorokey <80692846+dorokey@users.noreply.github.com>
2021-04-10 14:58:35 +0200

    adding video

--- Assets/Scripts/GeneralStuff/GlowStick.cs
+++ Assets/Scripts/GeneralStuff/GlowStick.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class GlowStick : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10f;
-    //[SerializeField] private float _superSpeed = 100f;
+    [SerializeField] private float _speed = 0.01f;
+    [SerializeField] private float _superSpeed = 10f;
 
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        transform.Rotate(new Vector3(0f,0f,10f));
+        //float _locus = transform.position.y;
+        //_locus += _speed * Time.deltaTime;
+        //transform.Translate(new Vector3(x:transform.position.x, y:_locus, 0f));
         if (transform.position.y >= 80f)
         {
             Destroy(this.gameObject);
